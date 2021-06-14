5b932b95a2bff7333ac9da2a6303c92a46cc1471
Dinesh <kundudinesh007@gmail.com>
2021-06-14 11:09:56 +0530

    Setup app for Heroku

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 document.querySelector('.weather-form').addEventListener('submit', (e) => {
     e.preventDefault();
     const address = document.querySelector('input').value;
-    fetch(`http://localhost:3000/weather?address=${address}`).then((res) => {
+    fetch(`/weather?address=${address}`).then((res) => {
     res.json().then((data) => {
         if(data.error) {
             document.querySelector('#message-1').innerHTML = data.error;
